740b9c3622fd9cac4e3896ea133be32e6a51d29d
Sagar Saby <sabysagar@gmail.com>
2024-02-20 22:38:58 -0800

    Started modeling port network with graph data structure

--- src/transportation.rs
+++ src/transportation.rs
@@ -8,3 +8,34 @@ pub struct Port {
     // what ports it can send people to
     connections: Vec<Port>
 }
+
+/** Represents a graph of port connections */
+pub struct PortGraph {
+    ports: Vec<Port>,
+    connections: Vec<PortConnection>
+}
+
+impl PortGraph {
+    fn new() -> Self{
+        PortGraph {ports: vec![], connections: vec![]}
+    }
+
+    fn add_port(&mut self, port: Port) {
+        self.ports.push(port);
+    }
+
+    fn add_connection(&mut self, start_port: Port, end_port: Port) {
+        self.connections.push(PortConnection{start: start_port, end: end_port});
+    }
+
+    // gets possible destination ports of a port in graph
+    fn get_dest_ports(&self) {
+
+    }
+
+}
+
+struct PortConnection {
+    start: Port,
+    end: Port
+}
