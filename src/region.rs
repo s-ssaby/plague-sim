f7a3151b0bf312a2ace924d2152250cd6d428fa2
Sagar Saby <sabysagar@gmail.com>
2024-02-20 19:57:08 -0800

    Implemented basic traits for structs

--- src/region.rs
+++ src/region.rs
@@ -1,10 +1,15 @@
+use crate::transportation::Port;
+
 /** Represents a region of the world with a human population */
+#[derive(Debug, Clone, Default, PartialEq)]
 pub struct Region {
     name: String,
-    population: Population
+    population: Population,
+    ports: Vec<Port>
 }
 
 
+#[derive(Debug, Clone, Default, PartialEq)]
 /** Represents any group of people */
 struct Population {
     alive: u32,
--- src/transportation.rs
+++ src/transportation.rs
@@ -1,4 +1,5 @@
 /** Represents a specific site of travel, such as an airport/seaport */
+#[derive(Debug, Clone, Default, PartialEq)]
 pub struct Port {
     // maximum amount of transportation
     capacity: u32,
