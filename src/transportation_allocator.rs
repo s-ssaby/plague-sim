b04ec72d986a0b029b86f75d2f75650b74d49e3a
Sagar Saby <sabysagar@gmail.com>
2024-03-20 16:21:39 -0700

    Completes RandomTransportAllocator

--- src/transportation_allocator.rs
+++ src/transportation_allocator.rs
@@ -1,6 +1,6 @@
 // Responsible for calculating ways to allocate people to transportation
 
-use crate::{math_utils::pick_random, population::Population, region::{Port, Region, RegionID}};
+use crate::{math_utils::{get_random, pick_random}, population::Population, region::{Port, Region, RegionID}};
 
 
 /** Determines how to create a transport job when given a starting port and its region and where it can travel to */
@@ -12,12 +12,21 @@ pub trait TransportAllocator {
 }
 
 /** Randomly choose a port to travel to, and transport a random number of people up to the starting port's capacity */
+///
+/** Population transported reflects composition of starting region
+ * For example, this allocator will have a transport consisting of roughly 50% infected if starting region is also 50% infected */
 pub struct RandomTransportAllocator;
 
 impl TransportAllocator for RandomTransportAllocator {
     fn calculate_transport<'a>(&self, start_port: &Port, start_region: &Region, destination_port_choices: Vec<&Port>) -> Option<TransportJob> {
-        todo!()
-
+        let random_dest = pick_random(destination_port_choices);
+        random_dest.map(|dest| {
+            let random_pop = ((start_port.capacity + 1) as f64*get_random()) as u32;
+            let scale_factor = (random_pop as f64)/(start_region.population.get_total() as f64);
+            let transported_population = start_region.population.scale(scale_factor); 
+            // TODO! Change time calculation later
+            TransportJob {start_region: start_region.id, end_region: dest.region, population: transported_population, time: 5}
+        })
     }
 }
 
