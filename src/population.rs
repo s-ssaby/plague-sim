9ddcd377ef1354b59fe49896d63d6cc0840a054e
Sagar Saby <sabysagar@gmail.com>
2024-03-20 16:07:32 -0700

    Adds scale and scale_truncate to Population to allow scalar multiplication of Populations

--- src/population.rs
+++ src/population.rs
@@ -28,6 +28,32 @@ impl Population {
         Self {healthy: initial_pop, dead: 0, recovered: 0, infected: 0}
     }
 
+    /// Creates a new population by scaling this population by a scalar factor
+    /// 
+    /// Note: Scaling will always round down (truncates)
+    /// 
+    /// Use scale for scaling operations that round to the nearest integer
+    pub fn scale_truncate(&self, scalar: f64) -> Population {
+        let new_healthy = (scalar*(self.healthy as f64)) as u32;
+        let new_dead = (scalar*(self.dead as f64)) as u32;
+        let new_recovered = (scalar*(self.recovered as f64)) as u32;
+        let new_infected = (scalar*(self.infected as f64)) as u32;
+        Self { healthy: new_healthy, infected: new_infected, dead: new_dead, recovered: new_recovered }
+    }
+
+    /// Creates a new population by scaling this population by a scalar factor
+    /// 
+    /// Note: Scaling will always round to the nearest integer
+    /// 
+    /// Use scale_truncate for scaling operations that always round down (truncation)
+    pub fn scale(&self, scalar: f64) -> Population {
+        let new_healthy = (scalar*(self.healthy as f64)).round() as u32;
+        let new_dead = (scalar*(self.dead as f64)).round() as u32;
+        let new_recovered = (scalar*(self.recovered as f64)).round() as u32;
+        let new_infected = (scalar*(self.infected as f64)).round() as u32;
+        Self { healthy: new_healthy, infected: new_infected, dead: new_dead, recovered: new_recovered }
+    }
+
 
     /* Returns all non-dead people in population */
     pub fn get_alive(&self) -> u32 {
@@ -60,3 +86,31 @@ impl Population {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::Population;
+
+    #[test]
+    fn scale_truncate() {
+        let population = Population {healthy: 150, infected: 75, dead: 111, recovered: 2};
+        let halved_population = population.scale_truncate(0.5);
+        let expected_population = Population {healthy: 75, infected: 37, dead: 55, recovered: 1};
+        assert_eq!(halved_population, expected_population);
+
+        let trisected_population = population.scale_truncate(0.333333);
+        let expected_population = Population {healthy: 49, infected: 24, dead: 36, recovered: 0};
+        assert_eq!(trisected_population, expected_population);
+    }
+
+    #[test]
+    fn scale() {
+        let population = Population {healthy: 150, infected: 75, dead: 111, recovered: 2};
+        let halved_population = population.scale(0.5);
+        let expected_population = Population {healthy: 75, infected: 38, dead: 56, recovered: 1};
+        assert_eq!(halved_population, expected_population);
+
+        let trisected_population = population.scale(0.333333);
+        let expected_population = Population {healthy: 50, infected: 25, dead: 37, recovered: 1};
+        assert_eq!(trisected_population, expected_population);
+    }
+}
