845e14dfba560a7995598dcf8b03689c96a02045
Sagar Saby <sabysagar@gmail.com>
2024-03-25 13:14:33 -0700

    Makes SimulationGeography generic

--- src/simulation_geography.rs
+++ src/simulation_geography.rs
@@ -1,23 +1,23 @@
-use crate::{region::{Port, PortID, Region}, transportation_graph::PortGraph};
+use crate::{location::{Location, Point2D}, region::{Port, PortID, Region}, transportation_graph::PortGraph};
 
 /// Responsible for storing simulation geography data and communicating changes across its components
 /// 
 /// Assumes that every port in the graph has a unique ID
 /// 
 /// Assumes that every port in all the regions has a unique ID
-pub struct SimulationGeography {
-    graph: PortGraph,
-    regions: Vec<Region>
+pub struct SimulationGeography<T = Point2D> where T: Location {
+    graph: PortGraph<T>,
+    regions: Vec<Region<T>>
 }
 
 // Invariants:
 // If a port with a certain ID exists in both graph and regions, their states must be equal
-impl SimulationGeography {
-    pub fn new(graph: PortGraph, regions: Vec<Region>) -> Self {
+impl<T> SimulationGeography <T> where T: Location {
+    pub fn new(graph: PortGraph<T>, regions: Vec<Region<T>>) -> Self {
         Self { graph, regions }
     }
 
-    fn find_port_in_regions(&self, port_id: PortID) -> Option<&Port> {
+    fn find_port_in_regions(&self, port_id: PortID) -> Option<&Port<T>> {
         for region in &self.regions {
             let result = region.ports.iter().find(|port| port.id == port_id);
             if result.is_some() {
@@ -28,7 +28,7 @@ impl SimulationGeography {
     }
 
     /* Find port with given ID, if it exists */
-    pub fn get_port(&self, port_id: PortID) -> Option<&Port> {
+    pub fn get_port(&self, port_id: PortID) -> Option<&Port<T>> {
         self.graph.get_port(port_id)
     }
 
