02a9586c636175350d09298a29bd216b7df05eec
Sagar Saby <sabysagar@gmail.com>
2024-03-25 20:10:20 -0700

    Adds ability to retrieve and modify Populations of Regions in SimulationGeography

--- src/simulation_geography.rs
+++ src/simulation_geography.rs
@@ -1,6 +1,6 @@
-use std::slice::Iter;
+use std::{fmt::format, slice::Iter};
 
-use crate::{location::{Location, Point2D}, region::{Port, PortID, Region, RegionID}, transportation_graph::PortGraph};
+use crate::{location::{Location, Point2D}, population::Population, region::{Port, PortID, Region, RegionID}, transportation_graph::PortGraph};
 
 /// Responsible for storing simulation geography data and communicating changes across its components
 /// 
@@ -31,6 +31,31 @@ impl<T> SimulationGeography <T> where T: Location {
         None
     }
 
+    /* Find region with given ID, if it exists */
+    pub fn get_region(&self, region_id: RegionID) -> Option<&Region<T>> {
+        self.regions.iter().find(|region| region.id == region_id)
+    }
+
+    fn get_region_mut(&mut self, region_id: RegionID) -> Option<&mut Region<T>> {
+        self.regions.iter_mut().find(|region| region.id == region_id)
+    }
+
+    /* Find port with given ID, if it exists */
+    pub fn get_port(&self, port_id: PortID) -> Option<&Port<T>> {
+        self.graph.get_port(port_id)
+    }
+
+
+    /* Returns population of specified region, if it exists */
+    pub fn get_population(&self, region_id: RegionID) -> Option<Population> {
+        self.get_region(region_id).map(|region| region.population)
+    }
+
+    /* Set population of specified region, if it exists */
+    pub fn set_population(&mut self, region_id: RegionID, population: Population) -> Result<(), String> {
+        self.get_region_mut(region_id).map(|region| region.population = population).ok_or(format!("Cannot find region ID {}", region_id))
+    }
+
     /* Returns contained regions */
     pub fn get_regions(&self) -> Iter<'_, Region<T>> {
         self.regions.iter()
@@ -49,16 +74,6 @@ impl<T> SimulationGeography <T> where T: Location {
     /* Gets open destination ports of a port, if it exists */
     pub fn get_open_dest_ports(&self, id: PortID) -> Option<Vec<&Port<T>>> {
         self.graph.get_open_dest_ports(id)
-    }
-
-    /* Find region with given ID, if it exists */
-    pub fn get_region(&self, region_id: RegionID) -> Option<&Region<T>> {
-        self.regions.iter().find(|region| region.id == region_id)
-    }
-
-    /* Find port with given ID, if it exists */
-    pub fn get_port(&self, port_id: PortID) -> Option<&Port<T>> {
-        self.graph.get_port(port_id)
     }
 
     /* Closes port with given ID, if it exists  */
