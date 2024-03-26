5488cd23c8915249f61ab7d4b6a50fe9e6190a68
Sagar Saby <sabysagar@gmail.com>
2024-03-25 18:45:47 -0700

    Finished adding accessor methods to SimulationGeography

--- src/simulation_geography.rs
+++ src/simulation_geography.rs
@@ -5,6 +5,8 @@ use crate::{location::{Location, Point2D}, region::{Port, PortID, Region, Region
 /// Assumes that every port in the graph has a unique ID
 /// 
 /// Assumes that every port in all the regions has a unique ID
+/// 
+/// Assumes that all ports contained in the regions are the same as all the ports in the graph
 pub struct SimulationGeography<T = Point2D> where T: Location {
     graph: PortGraph<T>,
     regions: Vec<Region<T>>
@@ -30,6 +32,21 @@ impl<T> SimulationGeography <T> where T: Location {
     /* Returns an iterator over contained regions */
     pub fn get_regions(&self) -> impl IntoIterator + '_ {
         &self.regions
+    }
+
+    /* Returns an iterator over contained ports */
+    pub fn get_ports(&self) -> impl IntoIterator + '_ {
+        self.graph.get_ports()
+    }
+
+    /* Gets possible destination ports of a port, if it exists */
+    pub fn get_all_dest_ports(&self, id: PortID) -> Option<Vec<&Port<T>>> {
+       self.graph.get_dest_ports(id)
+    }
+
+    /* Gets open destination ports of a port, if it exists */
+    pub fn get_open_dest_ports(&self, id: PortID) -> Option<Vec<&Port<T>>> {
+        self.graph.get_open_dest_ports(id)
     }
 
     /* Find region with given ID, if it exists */
