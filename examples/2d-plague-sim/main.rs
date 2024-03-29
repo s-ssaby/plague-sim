f0e04aedd8686637713c9971d53b85c91847d4bc
Sagar Saby <sabysagar@gmail.com>
2024-03-28 19:16:37 -0700

    Finished basic example simulation

--- examples/2d-plague-sim/main.rs
+++ examples/2d-plague-sim/main.rs
@@ -1,4 +1,69 @@
-extern crate functionality;
-fn main () {
-    println!("This is an example");
+use std::time::Duration;
+
+use functionality::{config::load_config_data, location::Point2D, region::RegionID, simulation_geography::SimulationGeography, transportation_allocator::RandomTransportAllocator};
+use macroquad::{miniquad::window::set_window_size, prelude::*};
+use simulation::Simulation;
+mod simulation;
+
+#[macroquad::main("Simulation")]
+async fn main() {
+    set_window_size(500, 500);
+    let config_data = load_config_data("examples/2d-plague-sim/simulation_data.json").unwrap();
+    let graph = config_data.graph;
+    let regions = config_data.regions;
+
+    let mut simulation: Simulation<Point2D, RandomTransportAllocator> = Simulation::<Point2D, RandomTransportAllocator>::new(SimulationGeography::new(graph, regions), RandomTransportAllocator::new(0.01));
+
+    // TODO: Create a separate loop for simulation, let rendering be own loop
+
+    loop {
+        clear_background(BLUE);
+
+        // update
+        simulation.update();
+
+        // north america ID: 0
+        draw_rectangle(0.0, 0.0, 100.0, 100.0, GREEN);
+        draw_text(format!("{}", simulation.geography.get_region(RegionID(0)).unwrap().population.get_total()).as_str(), 0.0, 100.0, 50.0, BLACK);
+
+        // brazil: ID 1
+        draw_rectangle(0.0, 150.0, 200.0, 100.0, GREEN);
+        draw_text(format!("{}", simulation.geography.get_region(RegionID(1)).unwrap().population.get_total()).as_str(), 0.0, 150.0, 50.0, BLACK);
+
+        // asia: ID 2
+        draw_rectangle(400.0, 0.0, 100.0, 100.0, GREEN);
+        draw_text(format!("{}", simulation.geography.get_region(RegionID(2)).unwrap().population.get_total()).as_str(), 400.0, 100.0, 50.0, BLACK);
+
+        // africa: ID 3
+        draw_rectangle(250.0, 220.0, 100.0, 100.0, GREEN);
+        draw_text(format!("{}", simulation.geography.get_region(RegionID(3)).unwrap().population.get_total()).as_str(), 250.0, 220.0, 50.0, BLACK);
+
+
+        for port in simulation.geography.get_ports() {
+            let x = port.pos.x;
+            let y = port.pos.y;
+            draw_circle(x as f32, y as f32, 10.0, WHITE);
+        }
+
+
+        println!("Transit Population is: {}", simulation.statistics.in_transit.get_total());
+        println!("Region Population is: {}", simulation.statistics.region_population.get_total());
+        println!("Total Population is: {} ", simulation.statistics.in_transit.get_total() + simulation.statistics.region_population.get_total());
+
+        // render flying planes
+        for job in &simulation.ongoing_transport {
+            let color = Color::new(f32::min((job.job.population.get_total() as f32)/(1000 as f32), 1.0), 0.0, 0.0, 1.0);
+            let prog_percent = (job.job.time as f64)/(job.expected_time as f64);
+            let start_port = simulation.geography.get_port(job.job.start_port).unwrap();
+            let end_port = simulation.geography.get_port(job.job.end_port).unwrap();
+
+            let plane_x = start_port.pos.x + prog_percent*(end_port.pos.x - start_port.pos.x);
+            let plane_y = start_port.pos.y + prog_percent*(end_port.pos.y - start_port.pos.y);
+
+            let radius = (job.job.population.get_total() as f32)/100.0;
+            draw_circle(plane_x as f32, plane_y as f32, radius, color);
+        }
+        std::thread::sleep(Duration::from_millis(16));
+        next_frame().await
+    }
 }
---
+++ examples/2d-plague-sim/simulation.rs
@@ -0,0 +1,131 @@
+use std::collections::HashMap;
+
+use functionality::{location::Location, population_types::population::Population, region::{Region, RegionID}, simulation_geography::SimulationGeography, transportation_allocator::{TransportAllocator, TransportJob}};
+
+
+
+/** Stores data not necessary for mediator's functioning, but may be useful for clients */
+pub struct MediatorStatistics {
+    /** Total population currently in transit */
+    pub in_transit: Population,
+    /** Total population living in regions */
+    pub region_population: Population
+}
+
+impl MediatorStatistics {
+    fn new (region_population: Population) -> Self {
+        Self { in_transit: Population::new(0), region_population }
+    }
+}
+
+// Controls transportation interactions between the regions it possesses
+/** Assumes that every port in provided port graph belongs to a region */
+/** Once regions added, cannot add more or take away */
+pub struct Simulation<A, T> where A: Location, T: TransportAllocator<A>{
+    pub geography: SimulationGeography<A>,
+    allocator: T,
+    pub ongoing_transport: Vec<InProgressJob>,
+    pub statistics: MediatorStatistics
+}
+
+impl<'a, A: Location + 'a, T: TransportAllocator<A>> Simulation<A, T> {
+    pub fn new(geography: SimulationGeography<A>, allocator: T) -> Self {
+        let total_pop = Self::calculate_regions_population(geography.get_regions());
+        Self {geography, ongoing_transport: vec![], statistics: MediatorStatistics::new(total_pop), allocator}
+    }
+
+    /** Calculates population contained in simulation's regions */
+    fn calculate_regions_population (regions: impl Iterator<Item = &'a Region<A>>) -> Population {
+        regions.map(|reg| reg.population).fold(Population::new(0), |acc, pop| acc + pop)
+    }
+
+    /** Calculates population currently in transit */
+    fn calculate_transit_population (jobs: impl Iterator<Item = &'a InProgressJob>) -> Population {
+        jobs.map(|job| job.job.population).fold(Population::new(0), |acc, pop| acc + pop)
+    }
+
+    /** Updates statistics of simulation to reflect current state */
+    fn update_statistics(&mut self) {
+        self.statistics.in_transit = Self::calculate_transit_population(self.ongoing_transport.iter());
+        self.statistics.region_population = Self::calculate_regions_population(self.geography.get_regions());
+    }
+
+    // create interactions between regions for each region
+    // also updates populations of regions when people leave
+    pub fn update(&mut self) {
+        // process jobs
+        self.ongoing_transport.retain_mut(|job| {
+            if job.job.time == 0 {
+                // update end region
+                let end_region = self.geography.get_region(job.job.end_region);
+                match end_region {
+                    Some(unwrapped_end_reg) => {
+                        self.geography.add_population(unwrapped_end_reg.id, job.job.population);
+                        return  false;
+                    },
+                    None => panic!("{}", format!("Region with ID {} that job is referring to doesn't exist in mediator", job.job.end_region)),
+                }
+            } else {
+                job.job.time -= 1;
+                return true;
+            }
+        });
+
+        let mut all_new_jobs: Vec<InProgressJob> = vec![];
+
+        // generate new jobs
+        for region in self.geography.get_region_ids() {
+            let new_jobs = Self::calculate_transport_jobs(&mut self.geography, region, &self.allocator);
+            all_new_jobs.extend(new_jobs);
+        }
+
+
+        self.ongoing_transport.extend(all_new_jobs);
+
+        // update stats
+        self.update_statistics()
+    }
+
+    // calculate transport jobs for a region
+    fn calculate_transport_jobs(geography: &mut SimulationGeography<A>, region_id: RegionID, allocator: &T) -> Vec<InProgressJob> {
+        let mut new_jobs: Vec<InProgressJob> = vec![];
+        
+        {
+        let region = geography.get_region(region_id).unwrap();
+        // look at each port
+        for port in &region.ports {
+            // where can each port go to?
+            let port_dests = geography.get_open_dest_ports(port.id).unwrap();
+
+            // calculate transport jobs
+            let calculated_jobs = allocator.calculate_transport(port, region, port_dests);
+            for job in calculated_jobs.unwrap_or(vec![]) {
+                match region.population.emigrate(job.population) {
+                    Ok(new_pop) => {
+                        // assume transportation takes 2 days
+                        new_jobs.push(InProgressJob::new(job))
+                    },
+                    Err(e) => panic!("{}", e),
+                }
+            }
+        }
+        }
+
+        // now modify region
+        for job in &new_jobs {
+            geography.subtract_population(region_id, job.job.population);
+        }
+        new_jobs
+    }
+}
+
+pub struct InProgressJob {
+    pub job: TransportJob,
+    pub expected_time: u32
+}
+
+impl InProgressJob {
+    pub fn new(job: TransportJob) -> Self {
+        Self {expected_time: job.time, job}
+    }
+}
