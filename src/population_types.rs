7a7eb6e15e521f13f4a479132b6ba58509ead522
Sagar Saby <sabysagar@gmail.com>
2024-04-02 19:33:32 -0700

    Replaced PopulationType enum with PopulationType sealed trait to encapsulate idea of population representation for Population and PopulatedArea types

--- src/population_types.rs
+++ src/population_types.rs
@@ -3,7 +3,31 @@ use self::{populated_area::PopulatedArea, population::Population};
 pub mod populated_area;
 pub mod population;
 
-pub enum PopulationType {
-    Population(Population),
-    PopulatedArea(PopulatedArea)
+/// Represents a type's ability to represent a population of people
+/// 
+/// This trait may not be implemented by clients on their types
+pub trait PopulationType : private::Sealed {
+    fn population(&self) -> Population;
+}
+
+impl PopulationType for Population {
+    fn population(&self) -> Population {
+        *self
+    }
+}
+
+impl PopulationType for PopulatedArea {
+    fn population(&self) -> Population {
+        self.get_population()
+    }
+}
+
+mod private {
+    use super::{populated_area::PopulatedArea, population::Population};
+
+    pub trait Sealed {}
+
+    // Should cover all Population types specified in population_types module
+    impl Sealed for PopulatedArea {}
+    impl Sealed for Population {}
 }
