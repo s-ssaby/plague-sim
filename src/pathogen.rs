ab15bea4f91015e91167d42bcb1af69caf8f1137
Sagar Saby <sabysagar@gmail.com>
2024-04-10 19:40:06 -0700

    Finish calculate_population for SpontaneousPathogen

--- src/pathogen.rs
+++ src/pathogen.rs
@@ -1,11 +1,15 @@
-use crate::population_types::{population::Population, Density, PopulationType};
+use crate::{math_utils::get_random, population_types::{population::Population, Density, PopulationType}};
 
 // Represents a pathogen, which are entities that transform populations without removing people from, or adding people to them
 pub trait Pathogen {
-    fn calculate_population<T>(&self, population: &T) -> T where T: PopulationType;
+    fn calculate_population<T>(&self, population: T) -> T where T: PopulationType;
 }
 
 /// Represents a pathogen that can spontaneously spawn into populations without any infected individuals
+/// Spontaneous generation occurs only when the following conditions hold:
+/// * At least one healthy individual exists in the population
+/// * No infected individuals exist in the population
+/// * Random chance allows its creation
 pub struct SpontaneousPathogen<T> where T: Pathogen {
     pub spawn_chance: f32,
     pub pathogen: T
@@ -18,8 +22,20 @@ impl<T> SpontaneousPathogen<T> where T: Pathogen {
 }
 
 impl<P> Pathogen for SpontaneousPathogen<P> where P: Pathogen {
-    fn calculate_population<T>(&self, population: &T) -> T where T: PopulationType {
-        todo!()
+    fn calculate_population<T>(&self, population: T) -> T where T: PopulationType {
+        let prev_population = population.population();
+        let new_population;
+        // spontaneous generation 
+        if prev_population.healthy > 0 && prev_population.infected == 0 && get_random() as f32 <= self.spawn_chance {
+            // spawn pathogen into population
+            new_population = Population {healthy: prev_population.healthy - 1, infected: 1, dead: prev_population.dead, recovered: prev_population.recovered};
+        } else {
+            // pathogen acts regularly
+            new_population = self.pathogen.calculate_population(prev_population);
+        }
+        let mut output_population = population;
+        output_population.set_population(new_population);
+        output_population
     }
 }
 
