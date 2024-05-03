e74fee43907a15c598d9af0e6cb3a8dde4b24bee
Sagar Saby <sabysagar@gmail.com>
2024-05-02 17:29:37 -0700

    Removes Location trait, moves relevant methods into Point2D struct

--- src/point.rs
+++ src/point.rs
@@ -1,13 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-/// Responsible for dealing with locations
-
-
-/// Represents a way to think of locations in the world
-/// Requires distance to be implemented to represent distances between instances of location
-pub trait Location : Clone {
-    fn distance(&self, second: &Self) -> f64;
-}
+/// Represents locations with a 2D Point
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Point2D {
@@ -19,9 +12,7 @@ impl Point2D {
     pub fn new (x: f64, y: f64) -> Self {
         Self {x, y}
     }
-}
 
-impl Location for Point2D {
     fn distance(&self, second: &Self) -> f64 {
         f64::sqrt((self.x - second.x)*(self.x - second.x) + (self.y - second.y)*(self.y - second.y))
     }
