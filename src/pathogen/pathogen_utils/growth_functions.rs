ec26c4ec3b53d0df29be107ab0c58e8d86b8b3f1
Sagar Saby <sabysagar@gmail.com>
2024-05-16 13:29:03 -0700

    Creates linear and exponential spread growth function templates

--- src/pathogen/pathogen_utils/growth_functions.rs
+++ src/pathogen/pathogen_utils/growth_functions.rs
@@ -1,1 +1,27 @@
+use crate::population_types::population::Population;
+
 /// Functions responsible for calculating pathogen spread growth
+
+
+/// Multiplies infected population by (1 + rate)
+/// Also decreases healthy population by same amount infected population was increased
+/// If unable to increase infected population by requested amount, increase as much as possible
+/// Always rounds down (truncated) new infections calculated
+pub fn exponential_spread_truncate(init_pop: Population, rate: f64) -> Population {
+    todo!();
+}
+
+/// Multiplies infected population by (1 + rate)
+/// Also decreases healthy population by same amount infected population was increased
+/// If unable to increase infected population by requested amount, increase as much as possible
+/// Always rounds down or up to nearest integer for new infections calculated
+pub fn exponential_spread_rounded(init_pop: Population, rate: f64) -> Population {
+    todo!();
+}
+
+/// Grows infected population by a constant amount
+/// Also decreased healthy population by same amount infected population was increased
+/// If unable to increase infected population by requested amount, increase as much as possible
+pub fn linear_spread(init_pop: Population, amount: u32) -> Population {
+    todo!()
+}
